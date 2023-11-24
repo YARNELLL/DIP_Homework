3066562e6b68785514d90df5e661a49e6742468e
YARNELLL <59241267+YARNELLL@users.noreply.github.com>
2023-11-24 13:58:26 +0800

    11月24日13:58电脑2修改

---
+++ Homework8/enhance.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+
+def opening_and_closing(input_img):
+    """
+
+    :param input_img:
+    :return:
+    """
+
+
+if __name__ == "__main__":
+    # 图片路径
+    img_fingerprint_path = "./image/fingerprint.tif"
+    # 读取图片
+    img_fingerprint = cv2.imread(img_fingerprint_path, 0)
